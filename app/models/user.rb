2e42c620183e6d48827bf783b5828a6f7849c076
peter-keller <keller.f.peter@gmail.com>
2019-04-17 11:46:10 +0100

    working implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :posts
+  
 
   has_one_attached :avatar
+
+
+  has_many :follower_follows, foreign_key: "followee_id", class_name: "Follower" 
+  has_many :followers, through: :follower_follows
+
+  has_many :followee_follows, foreign_key: "follower_id", class_name: "Follower"    
+  has_many :followees, through: :followee_follows
 end
